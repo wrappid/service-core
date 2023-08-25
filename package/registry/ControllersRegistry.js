f7ec2929f4f034e9a0a481d1b91bd01931308fe3
Pritam Mondal <pritammondal8981@gmail.com>
2023-08-25 15:59:00 +0530

    + code cleanup

--- package/registry/ControllersRegistry.js
+++ package/registry/ControllersRegistry.js
@@ -1,5 +1,6 @@
 const ControllersRegistry = {
-    "loginWithPassword": (req, res)=>{return res.status(200).json({message:"Login with password API called."})}
+    "loginWithPassword": (req, res)=>{return res.status(200).json({message:"Login with password API called."})},
+    "checkLoginOrRegister": (req, res)=>{return res.status(200).json({message:"checkLoginOrRegister."})},
 };
 
 module.exports = ControllersRegistry;
