65bf6b98744ef2cddeeae333d08ca470968ec6de
Nilotpal Saha <nilotpal.saha00@gmail.com>
2023-08-28 11:55:20 +0530

    + jwtVerfy middleware pushed

--- package/registry/MiddlewaresRegistry.js
+++ package/registry/MiddlewaresRegistry.js
@@ -1,10 +1,12 @@
 const apiLogger = require("../middlewares/apiLogger.middleware");
 const fileHandler = require("../middlewares/fileHandler.middleware");
+const jwtVerify = require("../middlewares/jwtVerify.middleware");
 const validation = require("../middlewares/validation.middleware");
 
 const MiddlewaresRegistry = {
     "apiLogger": apiLogger,
     "fileHandler": fileHandler,
+    "jwtVerify": jwtVerify,
     "validation": validation,
 };
 
