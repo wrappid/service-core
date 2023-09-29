a73e6a6f87cfdac077e2ee85dad547a4a4245311
Pritam Mondal <pritammondal8981@gmail.com>
2023-09-29 17:46:17 +0530

    + Fix bug

--- package/communication/communicate.communicator.js
+++ package/communication/communicate.communicator.js
@@ -1,5 +1,6 @@
 const { constant } = require("../constants/server.constant");
 const databaseActions = require("../database/actions.database");
+const { FunctionsRegistry } = require("../registry");
 const communicationUtils = require("../utils/communication.utils");
 const communicateEmail = require("./email/email.communication");
 const communicateSMS = require("./sms/sms.communication");
@@ -9,7 +10,7 @@ const communicateWhatsApp = require("./whatsapp/whatsapp.communication");
  * 
  * @param {*} communicationObject
  */
-const communicate = async ({ commType, commRecipients, commData, commTemplateID, directFlag, errorFlag }) => {
+const communicate = async ( {commType, commRecipients, commData, commTemplateID, directFlag, errorFlag} ) => {
   try {
     // get template
     let communicationTemplate = await databaseActions.findOne("application", "CommunicationTemplates", {
@@ -23,25 +24,33 @@ const communicate = async ({ commType, commRecipients, commData, commTemplateID,
     }
 
     let messageObject = communicationUtils.getMessageObject({ communicationTemplate, commData });
-
-    if (directFlag) {
-      switch (commType) {
-        case constant.commType.EMAIL:
-          return communicateEmail({ ...commRecipients, ...messageObject });
-        case constant.commType.SMS:
-          return communicateSMS({
-            phone: commRecipients.to[0],
-            ...messageObject,
-            dlttemplateid: communicationTemplate.externalTemplateId
-          });
-        case constant.commType.WHATSAPP:
-          return communicateWhatsApp();
-        default:
-          throw new Error("Communication type is invalid.");
+//create
+   FunctionsRegistry.createCommunicationHistory();
+    try {
+      if (directFlag) {
+        switch (commType) {
+          case constant.commType.EMAIL:
+            return communicateEmail({ ...commRecipients, ...messageObject });
+          case constant.commType.SMS:
+            return communicateSMS({
+              phone: commRecipients.to[0],
+              ...messageObject,
+              dlttemplateid: communicationTemplate.externalTemplateId
+            });
+          case constant.commType.WHATSAPP:
+            return communicateWhatsApp();
+          default:
+            throw new Error("Communication type is invalid.");
+        }
+        //check communcation status 
+        ///update
+      } else {
+        // db entry
       }
-    } else {
-      // db entry
+    } catch (error) {
+      //Update communication status fails
     }
+
   } catch (error) {
     console.error(error);
     if (errorFlag) {
