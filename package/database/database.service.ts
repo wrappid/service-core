8a231923177a3e08ed2d921ff96f75b4f2d2b602
PritamIT2023 <pritammondal8981@email.com>
2023-11-28 11:50:29 +0530

    databaseAction fix

--- package/database/database.service.ts
+++ package/database/database.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "../config/config.service";
-import { ConfigConstant } from "../constant/config.constant";
 import { Model, Sequelize } from "sequelize-typescript";
-import sequelize from "sequelize";
+import { Users } from "../modules/users/models/user.model";
 
 @Injectable()
 export class DatabaseService {
@@ -12,7 +11,7 @@ export class DatabaseService {
     databases.forEach(
       (
         database:
-           {
+          | {
               name: string;
               host: string;
               port: number;
@@ -27,22 +26,21 @@ export class DatabaseService {
          * @todo need to avoid above any type
          */
       ) => {
-        let seqObj:Sequelize = new Sequelize(database);
-            this.connections.set(database?.name, seqObj);
-            // sequelize.addModels([]);
-            // await sequelize.sync();
-            // return sequelize;
+        let seqObj: Sequelize = new Sequelize(database);
+        this.connections.set(database?.name, seqObj);
+        // sequelize.addModels([]);
+        // await sequelize.sync();
+        // return sequelize;
       }
     );
   }
-
 
   static getAllDatabaseProviders() {
     let databases = ConfigService.getCustomConfig()["databases"] || [];
     return databases.map(
       (
         database:
-           {
+          | {
               name: string;
               host: string;
               port: number;
@@ -72,10 +70,10 @@ export class DatabaseService {
     );
   }
 
-   getConnection(connectionName: string): Sequelize | undefined {
+  getConnection(connectionName: string): Sequelize | undefined {
     return this.connections.get(connectionName);
   }
-  async checkConnection(connection:Sequelize): Promise<boolean> {
+  async checkConnection(connection: Sequelize): Promise<boolean> {
     try {
       await connection.authenticate();
       // console.log("Database connection has been established successfully.",);
@@ -86,61 +84,69 @@ export class DatabaseService {
     }
   }
 
-  addModels(models: Model[], database: string){
-    models.forEach(element => {
-      
-    });
+  async addModels(models: Model[], connectionName: string) {
+    const dbObj = this.connections.get(connectionName);
+    /**
+     * @todo
+     * models array passing problem
+     */
+    dbObj.addModels([Users]);
+    console.log(`===Models Added===`);
   }
-
-  async findAndCountAll(model: any, options?: any): Promise<any[]> {
+  async findAndCountAll(connectionName: string, model: string, options?: any) {
     try {
-      return model.findAndCountAll(options);
+      const datbaseProvider = this.connections.get(connectionName);
+      return await datbaseProvider.models[model].findAndCountAll(options);
     } catch (error: any) {
       throw new Error(error);
     }
   }
 
-  async findAll(model: any, options?: any): Promise<any[]> {
+  async findAll(connectionName: string, model: string, options?: any): Promise<any[]> {
     try {
-      return model.findAll(options);
+      const datbaseProvider = this.connections.get(connectionName);
+      return await datbaseProvider.models[model].findAll(options);
     } catch (error: any) {
       throw new Error(error);
     }
   }
 
-  async findOne(model: any, options?: any): Promise<any> {
+  async findOne(connectionName: string, model: string, options?: any): Promise<any> {
     try {
-      return model.findOne(options);
+      const datbaseProvider = this.connections.get(connectionName);
+      return await datbaseProvider.models[model].findOne(options);
     } catch (error: any) {
       throw new Error(error);
     }
   }
 
-  async delete(model: any, options?: any): Promise<number> {
+  async delete(connectionName: string, model: string, options?: any): Promise<number> {
     try {
-      const instance = await this.findOne(model, options);
-      if (instance) {
-        return instance.destroy();
-      }
+      const datbaseProvider = this.connections.get(connectionName);
+      return await datbaseProvider.models[model].destroy(options);
     } catch (error: any) {
       throw new Error(error);
     }
   }
 
-  async update(model: any, data: any, options?: any): Promise<any> {
+  async update(
+    connectionName: string,
+    model: string,
+    data?: any,
+    options?: any
+  ): Promise<any> {
     try {
-      const instance = await this.findOne(model, options);
-      if (instance) {
-        return instance.update(data);
-      }
+      const datbaseProvider = this.connections.get(connectionName);
+      return await datbaseProvider.models[model].update(data, options);
     } catch (error: any) {
       throw new Error(error);
     }
   }
 
-  async create(model: any, data: any): Promise<any> {
+  async create(connectionName: string, model: string, data?: any): Promise<any> {
     try {
-      return model.create(data);
+      const datbaseProvider = this.connections.get(connectionName);
+      return datbaseProvider.models[model].create(data);
     } catch (error: any) {
       throw new Error(error);
     }
--- package/modules/users/users.module.ts
+++ package/modules/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module, OnModuleInit } from "@nestjs/common";
-import { Users } from "./models/user.model";
 import { UsersController } from "./users.controller";
 import { UsersService } from "./users.service";
 import { DatabaseService } from "../../database/database.service";
 import { DatabaseModule } from "../../database/ database.module";
+import { Users } from "./models/user.model";
 import { Students } from "./models/students.model";
+import { InjectModel } from "@nestjs/sequelize";
 
 @Module({
     imports:[DatabaseModule],
@@ -12,18 +13,12 @@ import { Students } from "./models/students.model";
   providers: [UsersService],
 })
 export class UsersModule implements OnModuleInit {
-  constructor(private readonly databaseService:DatabaseService){}
-  onModuleInit() {
+  constructor(
+    private readonly databaseService:DatabaseService,
+    ){}
+  async onModuleInit() {
   console.log(`::===UsersModule has been Initialization===::`);
-  //get sequelize connection instance
-  const sequelize1 = DatabaseService.getConnection("wrappid-database1");
-  //Add Model for module and database specific
-  sequelize1.addModels([Users]);
-  //get sequelize connection instance
-  const sequelize2 = DatabaseService.getConnection("wrappid-database2");
-  //Add Model for module and database specific
-  sequelize2.addModels([Students]);
-
+  await this.databaseService.addModels([],'wrappid-database1');
   }
   
 }
