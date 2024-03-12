19f842392f851c39dd7d62d0cb4e20c3877074e6
PritamIT2023 <pritammondal8981@email.com>
2024-03-13 00:32:30 +0530

    fix: fix version not coming
    fix version not coming
    
    Ref: #98

--- package/utils/routes.utils.ts
+++ package/utils/routes.utils.ts
@@ -1,15 +1,15 @@
-export const  extractUrl = (str:string) =>{
-  // Regular expression to match "https://" or "http://" followed by anything
-  const urlRegex = /(https?:\/\/)(.+)/;
-  
+export const  extractUrl = (str: string) => {
+  // Regular expression to match "https://" or "http://", optionally preceded by anything
+  const urlRegex = /(?:.*)(https?:\/\/)(.+)/;
+
   // Use match() to find the first occurrence of the pattern
   const match = str.match(urlRegex);
-  
+
   // If a match is found, return the full URL
   if (match) {
-    return match[0]; // Return the entire matched group (including protocol)
+    return match[1] + match[2]; // Return the protocol + remaining URL
   }
-  
-  // If no match is found, return an empty string
-  return "";
+
+  // If no match is found, return the original string
+  return str;
 };
