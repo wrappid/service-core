4eedebb14b0631ecbc4453dbc979a8fb14d32a0b
PritamIT2023 <pritammondal8981@email.com>
2024-03-21 11:40:32 +0530

    feat(global): :zap: database action fix
    all database action add a parameter transaction
    
    Ref: #113

--- package/database/actions.database.ts
+++ package/database/actions.database.ts
@@ -2,37 +2,62 @@ import { databaseProvider } from "./provider.database";
 
 export const databaseActions = {
   /**
-   *
+   * 
+   * @param database 
+   * @param model 
+   * @param options 
+   * @param transaction 
+   * @returns 
    */
-  findAndCountAll: async (database: string, model: string, options?: any) => {
+  findAndCountAll: async (database: string, model: string, options?: any, transaction?: any ) => {
     try {
-      return await databaseProvider[database].models[model].findAndCountAll(
-        options
-      );
+      return await databaseProvider[database].models[model].findAndCountAll(options, transaction);
     } catch (error: any) {
       throw new Error(error);
     }
   },
 
   /**
-   *
+   * 
+   * @param database 
+   * @param model 
+   * @param options 
+   * @param transaction 
+   * @returns 
    */
-  findAll: async (database: string, model: string, options?: any) => {
+  findAll: async (database: string, model: string, options?: any, transaction?: any) => {
     try {
-      return await databaseProvider[database].models[model].findAll(options);
+      return await databaseProvider[database].models[model].findAll(options, transaction);
     } catch (error: any) {
       throw new Error(error);
     }
   },
 
-  delete: async (database: string, model: string, data: any) => {
+  /**
+   * 
+   * @param database 
+   * @param model 
+   * @param data 
+   * @param transaction 
+   * @returns 
+   */
+  delete: async (database: string, model: string, data: any, transaction?: any) => {
     try {
-      return await databaseProvider[database].models[model].destroy(data);
+      return await databaseProvider[database].models[model].destroy(data, transaction);
     } catch (error: any) {
       throw new Error(error);
     }
   },
 
+  /**
+   * 
+   * @param database 
+   * @param model 
+   * @param data 
+   * @param where 
+   * @param transaction 
+   * @returns 
+   */
   update: async (
     database: string,
     model: string,
@@ -42,24 +67,38 @@ export const databaseActions = {
   ) => {
     try {
       return await databaseProvider[database].models[model].update(
-        data,
-        where,
-        transaction
+        data, where, transaction
       );
     } catch (error: any) {
       throw new Error(error);
     }
   },
 
-  findOne: async (database: string, model: string, data: any) => {
+  /**
+   * 
+   * @param database 
+   * @param model 
+   * @param data 
+   * @param transaction 
+   * @returns 
+   */
+  findOne: async (database: string, model: string, data: any, transaction?: any) => {
     try {
       console.log("::---", data, "---::");
-      return await databaseProvider[database].models[model].findOne(data);
+      return await databaseProvider[database].models[model].findOne(data, transaction);
     } catch (error: any) {
       throw new Error(error);
     }
   },
 
+  /**
+   * 
+   * @param database 
+   * @param model 
+   * @param data 
+   * @param transaction 
+   * @returns 
+   */
   create: async (
     database: string,
     model: string,
@@ -67,20 +106,24 @@ export const databaseActions = {
     transaction?: any
   ) => {
     try {
-      return await databaseProvider[database].models[model].create(
-        data,
-        transaction
-      );
+      return await databaseProvider[database].models[model].create(data, transaction);
     } catch (error: any) {
       throw new Error(error);
     }
   },
 
-  findByPk: async (database: string, model: string, primaryKey: number, options?: any) => {
+  /**
+   * 
+   * @param database 
+   * @param model 
+   * @param primaryKey 
+   * @param options 
+   * @param transaction 
+   * @returns 
+   */
+  findByPk: async (database: string, model: string, primaryKey: number, options?: any,  transaction?: any) => {
     try {
-      return await databaseProvider[database].models[model].findByPk(
-        primaryKey, options
-      );
+      return await databaseProvider[database].models[model].findByPk(primaryKey, options, transaction);
     } catch (error: any) {
       throw new Error(error);
     }
