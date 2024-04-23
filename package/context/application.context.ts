368d7fbff3a31226062d79269922bc697c302932
PritamIT2023 <pritammondal8981@email.com>
2024-04-23 12:58:42 +0530

    fix(global): :fire: fix object type
    fix object type
    
    Ref: #132

--- package/context/application.context.ts
+++ package/context/application.context.ts
@@ -1,5 +1,6 @@
-import { GenericObject } from "types/generic.types";
-
+type GenericObject = {
+  [key: string]: any;
+};
 
 export class ApplicationContext {
   private static context: GenericObject = {};
@@ -12,13 +13,4 @@ export class ApplicationContext {
     ApplicationContext.context[key] = value;
   }
 }
-  
-// // Usage
-// const key = "abdm";
-// const userData = "dfdfdf";
-  
-// ApplicationContext.setContext(key, userData);
-  
-// const retrievedData = ApplicationContext.getContext()[key];
-// console.log(retrievedData); // Output: { name: "Alice", role: "admin" }
-  
+ 
