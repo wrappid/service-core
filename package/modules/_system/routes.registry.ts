282b948bc69e50012b05529d75929559302397c1
PritamIT2023 <pritammondal8981@gmail.com>
2024-10-22 14:37:11 +0530

    fix(core): :bug: swagger api docs not generated fix
    swagger api docs not generated fix
    
    Ref: #191

--- package/modules/_system/routes.registry.ts
+++ package/modules/_system/routes.registry.ts
@@ -1,230 +1,474 @@
 const _systemRoutesRegistry = {
   getVersion: {
-    name: "Get Version API",
+    title: "Get Version API",
     url: "version",
     authRequired: false,
     entityRef: "getVersion",
     reqMethod: "get",
     controllerRef: "getVersion",
-    system: true
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ],
+    }
   },
   getDatabases:{
-    name: "Get Databases",
+    title: "Get Databases",
     url: "business/databases",
     authRequired: true,
     entityRef: "getDatabases",
     reqMethod: "get",
     controllerRef: "getDatabases",
-    system: true
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ],
+      "security": [
+        {
+          "bearerAuth": [
+            "jwtbearer"
+          ]
+        }
+      ],
+    }
   },
   getDatabaseTables: {
-    name: "Get Database Tables",
+    title: "Get Database Tables",
     url: "business/tables/:database",
     authRequired: true,
     entityRef: "getDatabaseTables",
     reqMethod: "get",
     controllerRef: "getDatabaseTables",
-    system: true
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ],
+      "security": [
+        {
+          "bearerAuth": [
+            "jwtbearer"
+          ]
+        }
+      ],
+    }
   },
   getAttributes: {
-    name: "Get Table Attributes",
+    title: "Get Table Attributes",
     url: "business/tables/attributes/:database/:table",
     authRequired: true,
     entityRef: "getAttributes",
     reqMethod: "get",
     controllerRef: "getAttributes",
-    system: true
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ],
+      "security": [
+        {
+          "bearerAuth": [
+            "jwtbearer"
+          ]
+        }
+      ],
+    }
   },
   getBusinessEntityColumns: {
-    name: "Get Entity",
+    title: "Get Entity",
     url: "noauth/business/columns/:entity",
     authRequired: false,
     entityRef: "getBusinessEntityColumns",
     reqMethod: "get",
     controllerRef: "getBusinessEntityColumns",
-    system: true
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ]
+    }
   },
   getBusinessEntities: {
-    name: "Get Business Entities",
+    title: "Get Business Entities",
     url: "business/entities",
     authRequired: true,
     entityRef: "getBusinessEntities",
     reqMethod: "get",
     controllerRef: "getBusinessEntities",
-    system: true
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ],
+      "security": [
+        {
+          "bearerAuth": [
+            "jwtbearer"
+          ]
+        }
+      ],
+    }
   },
   getEntityData: {
-    name: "Get Entity Count",
+    title: "Get Entity Count",
     url: "business/count/:entity",
     authRequired: true,
     entityRef: "getEntityData",
     reqMethod: "get",
     controllerRef: "getEntityData",
-    system: true
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ],
+      "security": [
+        {
+          "bearerAuth": [
+            "jwtbearer"
+          ]
+        }
+      ],
+    }
   },
   getIndividualEntityData: {
-    name: "Get Individual EntityData",
+    title: "Get Individual EntityData",
     url: "business/individual/:entity",
     authRequired: true,
     entityRef: "getIndividualEntityData",
     reqMethod: "get",
     controllerRef: "getIndividualEntityData",
-    system: true
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ],
+      "security": [
+        {
+          "bearerAuth": [
+            "jwtbearer"
+          ]
+        }
+      ],
+    }
   },
   getAllEntityData: {
-    name: "Get All Entity Data",
+    title: "Get All Entity Data",
     url: "business/all/:entity",
     authRequired: true,
     entityRef: "getAllEntityData",
     reqMethod: "get",
     controllerRef: "getAllEntityData",
-    system: true
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ],
+      "security": [
+        {
+          "bearerAuth": [
+            "jwtbearer"
+          ]
+        }
+      ],
+    }
   },
   noAuthGetAllEntityData: {
-    name: "No Auth Get All Entity Data",
+    title: "No Auth Get All Entity Data",
     url: "noauth/business/all/:entity",
     authRequired: false,
     entityRef: "noAuthGetAllEntityData",
     reqMethod: "get",
     controllerRef: "noAuthGetAllEntityData",
-    system: true
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ]
+    }
   },
   getNoAuthFormSchema: {
-    name: "Get No Auth Form Schema",
+    title: "Get No Auth Form Schema",
     url: "noauth/formSchema/:formID",
     authRequired: false,
     entityRef: "getNoAuthFormSchema",
     reqMethod: "get",
     controllerRef: "getNoAuthFormSchema",
-    system: true
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ]
+    }
   },
   getFormSchema: {
-    name: "Form Schema",
+    title: "Form Schema",
     url: "formSchema/:formID",
     authRequired: true,
     entityRef: "formSchema",
     reqMethod: "get",
     controllerRef: "getFormSchema",
-    system: true
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ],
+      "security": [
+        {
+          "bearerAuth": [
+            "jwtbearer"
+          ]
+        }
+      ],
+    }
   },
   getSettingMeta:{
-    name: "Setting Meta",
+    title: "Setting Meta",
     url: "settingMeta",
     authRequired: true,
     entityRef: "settingMeta",
     reqMethod: "get",
     controllerRef: "getSettingMeta",
-    system: true
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ],
+      "security": [
+        {
+          "bearerAuth": [
+            "jwtbearer"
+          ]
+        }
+      ],
+    }
   },
   getModels: {
-    name: "Get Database Models",
+    title: "Get Database Models",
     url: "models/:database",
     authRequired: true,
     entityRef: "getModels",
     reqMethod: "get",
     controllerRef: "getModels",
-    system: true
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ],
+      "security": [
+        {
+          "bearerAuth": [
+            "jwtbearer"
+          ]
+        }
+      ],
+    }
   },
   getDatabaseModels: {
-    name: "Get database Models",
+    title: "Get database Models",
     url: "data/:model",
     authRequired: true,
     entityRef: "getDatabaseModels",
     reqMethod: "get",
     controllerRef: "getDatabaseModels",
-    system: true
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ],
+      "security": [
+        {
+          "bearerAuth": [
+            "jwtbearer"
+          ]
+        }
+      ],
+    }
   },
   getDatabaseModelRow: {
-    name: "Get Database Model Row by id ",
+    title: "Get Database Model Row by id ",
     url: "data/:model/:id",
     authRequired: true,
     entityRef: "getDatabaseModelRow",
     reqMethod: "get",
     controllerRef: "getDatabaseModelRow",
-    system: true
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ],
+      "security": [
+        {
+          "bearerAuth": [
+            "jwtbearer"
+          ]
+        }
+      ],
+    }
   },
   postDatabaseModel: {
-    name: "Create row in model",
+    title: "Create row in model",
     url: "data/:model",
     authRequired: true,
     entityRef: "postDatabaseModel",
     reqMethod: "post",
     controllerRef: "postDatabaseModel",
-    system: true
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ],
+      "security": [
+        {
+          "bearerAuth": [
+            "jwtbearer"
+          ]
+        }
+      ],
+    }
   },
   putDatabaseModel: {
-    name: "Update model",
+    title: "Update model",
     url: "data/:model/:id",
     authRequired: true,
     entityRef: "putDatabaseModel",
     reqMethod: "put",
     controllerRef: "putDatabaseModel",
-    system: true
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ],
+      "security": [
+        {
+          "bearerAuth": [
+            "jwtbearer"
+          ]
+        }
+      ],
+    }
   },
   putUpdateStatus: {
-    name: "Update Status",
+    title: "Update Status",
     url: "data/:model/status/:id",
     authRequired: true,
     entityRef: "putUpdateStatus",
     reqMethod: "put",
     controllerRef: "putUpdateStatus",
-    system: true
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ],
+      "security": [
+        {
+          "bearerAuth": [
+            "jwtbearer"
+          ]
+        }
+      ],
+    }
   },
   patchDatabaseModel: {
-    name: "Delete Database Model",
+    title: "Delete Database Model",
     url: "data/:model/:id",
     authRequired: true,
     entityRef: "patchDatabaseModel",
     reqMethod: "PATCH",
     controllerRef: "patchDatabaseModel",
-    system: true
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ],
+      "security": [
+        {
+          "bearerAuth": [
+            "jwtbearer"
+          ]
+        }
+      ],
+    }
   },
   postTestCommunication: {
-    name: "Test communication",
+    title: "Test communication",
     url: "communication/test/:commType",
     authRequired: false,
     entityRef: "postTestCommunication",
     reqMethod: "post",
     controllerRef: "postTestCommunication",
-    system: true
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ]
+    }
   },
   //Get Registry data
   getRegistry: {
-    name: "Get Registry Data",
-    url: "registry/:name",
+    title: "Get Registry Data",
+    url: "registry/:title",
     authRequired: false,
     entityRef: "getRegistry",
     reqMethod: "get",
     controllerRef: "getRegistry",
-    system: true
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ]
+    }
   },
   getRegistryList: {
-    name: "Get Registry list",
+    title: "Get Registry list",
     url: "regitry/list",
     authRequired: false,
     entityRef: "getRegistryList",
     reqMethod: "get",
     controllerRef: "getRegistryList",
-    system: true
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ]
+    }
   },
   //clone 
   postCloneDataModel: {
-    name: "Clone data model",
+    title: "Clone data model",
     url: "data/clone/:model/:entityRef",
     authRequired: true,
     entityRef: "postCloneDataModel",
     reqMethod: "post",
     controllerRef: "postCloneDataModel",
-    system: true
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ],
+      "security": [
+        {
+          "bearerAuth": [
+            "jwtbearer"
+          ]
+        }
+      ],
+    }
   },
   postDataModelSync: {
-    name: "Post data model sync",
+    title: "Post data model sync",
     url: "data/sync/:model",
     authRequired: false,
     entityRef: "postDataModelSync",
     reqMethod: "post",
     controllerRef: "postDataModelSync",
-    system: true
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ]
+    }
   }
 };
 export default _systemRoutesRegistry;
--- package/registry/RoutesRegistry.ts
+++ package/registry/RoutesRegistry.ts
@@ -1,29 +1,40 @@
 import _systemRoutesRegistry from "../modules/_system/routes.registry";
 
 const RoutesRegistry = {
-  getDatabases: {
-    name: "getDatabases",
-    url: "getAllDatabases",
-    authRequired: false,
-    entityRef: "testGetAllAPI",
-    reqMethod: "get",
-    controllerRef: "getDatabases",
-  },
   getTables: {
-    name: "getTables",
+    title: "Get database table",
     url: "getAllTables/:database",
     authRequired: false,
-    entityRef: "testGetAllAPI",
+    entityRef: "getTables",
     reqMethod: "get",
     controllerRef: "getTables",
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ],
+      "security": [
+        {
+          "bearerAuth": [
+            "jwtbearer"
+          ]
+        }
+      ],
+    }
   },
   getColumns: {
-    name: "getAllColumns/:database/:table",
+    title: "Get all",
     url: "wrappidall",
     authRequired: false,
-    entityRef: "testGetAllAPI",
+    entityRef: "getColumns",
     reqMethod: "get",
     controllerRef: "getColumns",
+    system: true,
+    swaggerJson: {
+      "tags": [
+        "service-core"
+      ]
+    }
   },
   ..._systemRoutesRegistry
 };
